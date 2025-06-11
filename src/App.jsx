df06ed75d96917d547e4efce4c38a74585a668b5
Mohamed waleed <m.walid@symstax.com>
2025-06-11 23:20:39 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,26 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./pages/home/home";
-import {
-  createBrowserRouter,
-  createHashRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/layout";
 import NotFound from "./layout/notfound";
 import About from "./pages/about/about";
 import Portfolio from "./pages/portfolio/portfolio";
 import Contact from "./pages/contact/contact";
 
-let rout = createHashRouter([
-  {
-    path: "",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "About", element: <About /> },
-      { path: "Portfolio", element: <Portfolio /> },
-      { path: "Contact", element: <Contact /> },
-      { path: "*", element: <NotFound /> },
-    ],
-  },
-]);
-
 function App() {
   return (
-    <>
-      <RouterProvider router={rout}></RouterProvider>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
