d3e7236da65ef5ab2bca43c347d7c98cea731876
Mohamed waleed <m.walid@symstax.com>
2025-06-11 23:13:32 +0300

    github pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./pages/home/home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./layout/layout";
 import NotFound from "./layout/notfound";
 import About from "./pages/about/about";
 import Portfolio from "./pages/portfolio/portfolio";
 import Contact from "./pages/contact/contact";
 
-let rout = createBrowserRouter([
+let rout = createHashRouter([
   {
     path: "",
     element: <Layout />,
@@ -23,6 +27,7 @@ let rout = createBrowserRouter([
     ],
   },
 ]);
+
 function App() {
   return (
     <>
